d59e9ff63fecc068284597aac750c5dee5e411cb
NaokiM03 <naokim03.lib+dev@gmail.com>
2022-12-29 01:21:24 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -3,52 +3,6 @@ use sysinfo::{get_current_pid, CpuExt, ProcessExt, System, SystemExt};
 use tiny_ansi::TinyAnsi;
 use wmi::{COMLibrary, Variant, WMIConnection};
 
-#[derive(Debug)]
-struct Content {
-    key: String,
-    padding: usize,
-    value: Vec<String>,
-}
-
-impl Content {
-    fn new(key: &str, value: &[&str]) -> Self {
-        Content {
-            key: key.to_owned(),
-            padding: 0,
-            value: value.iter().map(|x| x.to_string()).collect(),
-        }
-    }
-
-    fn build(&self) -> Vec<String> {
-        if self.value.len() == 1 {
-            vec![format!(
-                "{}: {}{}",
-                self.key.bright_blue(),
-                " ".repeat(self.padding),
-                self.value[0]
-            )]
-        } else {
-            let mut v = Vec::new();
-            self.value.iter().enumerate().for_each(|(i, x)| {
-                if i == 0 {
-                    v.push(format!(
-                        "{}: {}- {}",
-                        self.key.bright_blue(),
-                        " ".repeat(self.padding),
-                        self.value[0]
-                    ));
-                } else {
-                    v.push(format!(
-                        "{}  - {}",
-                        " ".repeat(self.key.len() + self.padding),
-                        x
-                    ));
-                }
-            });
-            v
-        }
-    }
-}
 fn get_logo() -> String {
     r#"
 ###############  ###############
@@ -70,19 +24,34 @@ fn get_logo() -> String {
     .trim()
     .to_owned()
 }
+const LOGO_WIDTH: usize = 32;
+
+fn create_label(s: &str) -> String {
+    let s = format!("{s}:");
+    format!("{s: <10}").bright_blue()
+}
+
+fn create_fake_label() -> String {
+    let empty_str = "";
+    format!("{empty_str: <10}")
+}
 
 fn main() {
     let mut sys = System::new_all();
     sys.refresh_all();
 
-    let user = format!(
-        "{}@{}",
-        whoami::username().bright_blue(),
-        whoami::hostname().bright_blue()
-    );
+    let user = {
+        let user_name = whoami::username().bright_blue();
+        let host_name = whoami::hostname().bright_blue();
+        format!("{user_name}@{host_name}")
+    };
     let under_line = "-".repeat(user.len());
 
-    let os = Content::new("OS", &vec![whoami::distro().as_str()]);
+    let os = {
+        let label = create_label("OS");
+        let os = whoami::distro();
+        format!("{label}{os}")
+    };
 
     let uptime = {
         const SECONDS_PER_MINUTE: u64 = 60;
@@ -99,14 +68,9 @@ fn main() {
         let remaining = remaining - hours * SECONDS_PER_HOUR;
         let minutes = remaining / SECONDS_PER_MINUTE;
 
-        Content::new(
-            "Uptime",
-            &vec![format!(
-                "{} weeks {} days {} hours {} minutes",
-                weeks, days, hours, minutes
-            )
-            .as_str()],
-        )
+        let label = create_label("Uptime");
+        let uptime = format!("{weeks} weeks {days} days {hours} hours {minutes} minutes");
+        format!("{label}{uptime}")
     };
 
     let shell = {
@@ -114,44 +78,56 @@ fn main() {
         let current_process = sys.process(current_process_pid).unwrap();
         let parent_process_pid = current_process.parent().unwrap();
         let parent_process = sys.process(parent_process_pid).unwrap();
-        Content::new(
-            "Shell",
-            &vec![match parent_process.name() {
-                "cmd.exe" => "CommandPrompt",
-                "powershell.exe" => "PowerShell",
-                "nu.exe" => "Nu",
-                _ => "Unkwon",
-            }],
-        )
+
+        let label = create_label("Shell");
+        let shell = match parent_process.name() {
+            "cmd.exe" => "CommandPrompt",
+            "powershell.exe" => "PowerShell",
+            "nu.exe" => "Nu",
+            _ => "Unkwon",
+        };
+        format!("{label}{shell}")
     };
 
-    let cpu = {
+    let mut cpu = {
+        let label = create_label("CPU");
+        let fake_label = create_fake_label();
         let cpu = sys.global_cpu_info().brand();
-        let cores_and_threads = &format!(
+        let cores_and_threads = format!(
             "{} Cores {} Threads",
             num_cpus::get_physical(),
             num_cpus::get(),
         );
-        Content::new("CPU", &vec![cpu, cores_and_threads])
+        vec![
+            format!("{label}- {cpu}"),
+            format!("{fake_label}- {cores_and_threads}"),
+        ]
     };
 
-    let gpu = {
+    let mut gpu = {
         let com_con = COMLibrary::new().unwrap();
         let wmi_con = WMIConnection::new(com_con.into()).unwrap();
         let result: Vec<HashMap<String, Variant>> = wmi_con
             .raw_query("SELECT Caption FROM Win32_VideoController")
             .unwrap();
-        let v = result
-            .iter()
-            .map(|x| {
-                let v = &*x.get("Caption").unwrap().to_owned();
-                match v {
-                    Variant::String(info) => info.as_str(),
-                    _ => "",
+        let gpus = result.iter().map(|x| {
+            let v = &*x.get("Caption").unwrap().to_owned();
+            match v {
+                Variant::String(info) => info.to_owned(),
+                _ => "".to_owned(),
+            }
+        });
+        gpus.enumerate()
+            .map(|(i, gpu)| {
+                if i == 0 {
+                    let label = create_label("GPU");
+                    format!("{label}- {gpu}")
+                } else {
+                    let fake_label = create_fake_label();
+                    format!("{fake_label}- {gpu}")
                 }
             })
-            .collect::<Vec<&str>>();
-        Content::new("GPU", &v)
+            .collect::<Vec<String>>()
     };
 
     let memory = {
@@ -160,21 +136,19 @@ fn main() {
             / 1024f64 // MiB
             / 1024f64 // GiB
             ;
-
         let total = sys.total_memory() as f64
             / 1024f64 // KiB
             / 1024f64 // MiB
             / 1024f64 // GiB
             ;
-
         let usage_rate = used / total * 100f64;
+
+        let label = create_label("Memory");
+        let memory = format!("{:.1}GB / {:.1} GB ({:.1}%)", used, total, usage_rate);
 
         // `Task Manager` apparently displays `GiB` values in `GB` units, so use `GB`.
         // NOTE: It seems that `systeminfo` also displays `MiB` values in `MB` units.
-        Content::new(
-            "Memory",
-            &vec![format!("{:.1}GB / {:.1} GB ({:.1}%)", used, total, usage_rate).as_str()],
-        )
+        format!("{label}{memory}")
     };
 
     let monitors = {
@@ -186,38 +160,35 @@ fn main() {
                 format!("{}x{}", physical_size.width, physical_size.height)
             })
             .collect();
-        Content::new("Monitors", &vec![resolutions.join(" ").as_str()])
+
+        let label = create_label("Monitors");
+        let monitors = resolutions.join(" ");
+        format!("{label}{monitors}")
     };
 
-    let mut info = Vec::new();
+    let mut right_content = Vec::new();
+    right_content.push(user);
+    right_content.push(under_line);
+    right_content.push(os);
+    right_content.push(uptime);
+    right_content.push(shell);
+    right_content.append(&mut cpu);
+    right_content.append(&mut gpu);
+    right_content.push(memory);
+    right_content.push(monitors);
 
-    info.push(user);
-    info.push(under_line);
+    let logo = get_logo();
+    let mut left_content: Vec<String> = logo.lines().map(|x| x.to_owned().bright_blue()).collect();
 
-    let mut contents = {
-        let mut v = vec![os, uptime, shell, cpu, gpu, memory, monitors];
-        let max_key_length = v.iter().map(|x| x.key.len()).max().unwrap();
-        v.iter_mut().for_each(|x| {
-            x.padding = max_key_length - x.key.len();
-        });
-        v.iter().map(|x| x.build()).flatten().collect()
-    };
-    info.append(&mut contents);
-
-    let mut logo: Vec<String> = get_logo().lines().map(|x| x.to_owned()).collect();
-
-    if logo.len() > info.len() {
-        info.resize(logo.len(), String::default());
-    } else if logo.len() < info.len() {
-        logo.resize(
-            info.len(),
-            " ".repeat(logo.iter().map(|x| x.len()).max().unwrap()),
-        );
+    if left_content.len() >= right_content.len() {
+        right_content.resize(left_content.len(), String::default());
+    } else {
+        left_content.resize(right_content.len(), String::default().repeat(LOGO_WIDTH));
     }
-
-    let logo: Vec<String> = logo.iter().map(|x| x.bright_blue()).collect();
-
-    logo.iter().zip(info.iter()).for_each(|(left, right)| {
-        println!("    {}  {}", left, right);
-    });
+    left_content
+        .iter()
+        .zip(right_content.iter())
+        .for_each(|(left, right)| {
+            println!("    {left}  {right}");
+        });
 }
